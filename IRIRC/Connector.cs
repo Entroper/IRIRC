2f8ecc9616e9e75566d806494bd0b42761b30796
Entroper <entroper@gmail.com>
2014-10-22 01:42:25 -0400

    connect to IRC

---
+++ IRIRC/Connector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ChatSharp;
+
+namespace IRIRC
+{
+	class Connector
+	{
+		private IrcClient _client;
+		private TaskCompletionSource<int> _connectTcs;
+
+		public async Task Start(string host, string username, string password, string channelName)
+		{
+			await Connect(host, username, password);
+
+			_client.Channels.Join(channelName.ToLower());
+
+			_client.ChannelMessageRecieved += OnChannelMessageRecieved;
+		}
+		
+		private Task Connect(string host, string username, string password)
+		{
+			_client = new IrcClient(host, new IrcUser(username.ToLower(), username, password));
+			_client.ConnectionComplete += OnConnectionComplete;
+			_connectTcs = new TaskCompletionSource<int>();
+			
+			_client.ConnectAsync();
+
+			return _connectTcs.Task;
+		}
+
+		void OnConnectionComplete(object sender, EventArgs e)
+		{
+			_connectTcs.SetResult(0);
+		}
+
+		void OnChannelMessageRecieved(object sender, ChatSharp.Events.PrivateMessageEventArgs e)
+		{
+			MessageBox.Show("<" + e.PrivateMessage.User.Nick + "> " + e.PrivateMessage.Message);
+		}
+	}
+}
--- IRIRC/MainWindow.xaml.cs
+++ IRIRC/MainWindow.xaml.cs
@@ -20,9 +20,33 @@ namespace IRIRC
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private Connector _connector = new Connector();
+
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void sameAsUsernameCheckBox_Checked(object sender, RoutedEventArgs e)
+		{
+			channelTextBox.Text = "#" + usernameTextBox.Text;
+			channelTextBox.IsEnabled = false;
+		}
+
+		private void sameAsUsernameCheckBox_Unchecked(object sender, RoutedEventArgs e)
+		{
+			channelTextBox.IsEnabled = true;
+		}
+
+		private void usernameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (sameAsUsernameCheckBox.IsChecked.HasValue && sameAsUsernameCheckBox.IsChecked.Value)
+				channelTextBox.Text = "#" + usernameTextBox.Text;
+		}
+
+		private async void connectButton_Click(object sender, RoutedEventArgs e)
+		{
+			await _connector.Start(ircServerTextBox.Text, usernameTextBox.Text, passwordBox.Password, channelTextBox.Text);
 		}
 	}
 }
